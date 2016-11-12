083ccdf8f35004800253ce3c3fe1d8f1adecc9fb
JohnWestwig <john.westwig@gmail.com>
2016-11-12 15:45:33 -0500

    Checked location permissions in main activity

--- TwoCentsApp/app/src/main/java/edu/tufts/cs/twocents/MainActivity.java
+++ TwoCentsApp/app/src/main/java/edu/tufts/cs/twocents/MainActivity.java
@@ -1,15 +1,21 @@
 package edu.tufts.cs.twocents;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.ndk.CrashlyticsNdk;
 import io.fabric.sdk.android.Fabric;
+
+import static java.security.AccessController.getContext;
 
 
 /**
@@ -22,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
 
+    private static final int MY_PERMISSIONS_ACCESS_LOCATION_FINE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,40 +38,59 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setupTabs();
+    }
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        tabLayout.setupWithViewPager(viewPager);
+    private void setupTabs() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+            }, MY_PERMISSIONS_ACCESS_LOCATION_FINE );
+        } else {
+            TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+            viewPager = (ViewPager) findViewById(R.id.pager);
+            tabLayout.setupWithViewPager(viewPager);
 
-        MainFragmentPagerAdapter adapter = new MainFragmentPagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
+            MainFragmentPagerAdapter adapter = new MainFragmentPagerAdapter(getSupportFragmentManager());
+            viewPager.setAdapter(adapter);
 
-        for (int i = 0; i < tabLayout.getTabCount(); i++) {
-            tabLayout.getTabAt(i).setIcon(ICON_IDS[i]);
+            for (int i = 0; i < tabLayout.getTabCount(); i++) {
+                tabLayout.getTabAt(i).setIcon(ICON_IDS[i]);
+            }
+
+            tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+            tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    viewPager.setCurrentItem(tab.getPosition(), true);
+                }
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+
+                }
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+
+                }
+            });
+
+            User user = new User(getApplicationContext());
+            if (!user.isUserKeySet()) {
+                Intent intent = new Intent(this, CreateUserActivity.class);
+                startActivity(intent);
+            }
         }
+    }
 
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition(), true);
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-        User user = new User(getApplicationContext());
-        if (!user.isUserKeySet()) {
-            Intent intent = new Intent(this, CreateUserActivity.class);
-            startActivity(intent);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_ACCESS_LOCATION_FINE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setupTabs();
+                }
         }
     }
 }
